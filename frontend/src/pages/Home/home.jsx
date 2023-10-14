e3f8cadec61ede847a271f27014abebc31086f95
svyatoslav <quiedcpp@gmail.com>
2023-10-14 17:30:38 +0300

    home - manga/anime section

--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -1,11 +1,115 @@
-import React from "react";
+import React, {useState} from "react";
+import ItemsCarousel from 'react-items-carousel';
 import Head from "../../components/head/head";
 import Styles from './home.module.scss';
+import MangaImage from '../../components/images/mang.jpg'; // TEST
+import Title1 from '../../components/images/title1.jpeg'; // TEST
+import Title2 from '../../components/images/title2.jpeg'; // TEST
+import Title3 from '../../components/images/title3.jpeg'; // TEST
+import Title4 from '../../components/images/title4.jpeg'; // TEST
+import Title5 from '../../components/images/title5.jpeg'; // TEST
+import Title6 from '../../components/images/title6.jpeg'; // TEST
+import Title7 from '../../components/images/title7.jpeg'; // TEST
+
+function AnimeSlider(){
+    const [activeItemIndex, setActiveItemIndex] = useState(0);
+    const chevronWidth = 150;
+
+    return(
+        <div className={Styles.Manga__Slider}>
+            <div style={{ padding: `0 ${chevronWidth}px` }}>
+                <ItemsCarousel
+                    requestToChangeActive={setActiveItemIndex}
+                    activeItemIndex={activeItemIndex}
+                    numberOfCards={5}
+                    gutter={20}
+                    leftChevron={<button className={Styles.SliderButton}>{'<'}</button>}
+                    rightChevron={<button className={Styles.SliderButton}>{'>'}</button>}
+                    outsideChevron
+                    chevronWidth={chevronWidth}
+                >
+                    <AnimeItem image={Title5}></AnimeItem>
+                    <AnimeItem image={Title7}></AnimeItem>
+                    <AnimeItem image={Title7}></AnimeItem>
+                    <AnimeItem image={Title6}></AnimeItem>
+                    <AnimeItem image={Title7}></AnimeItem>
+                    <AnimeItem image={Title7}></AnimeItem>
+                    <AnimeItem image={Title6}></AnimeItem>
+                    <AnimeItem image={Title7}></AnimeItem>
+                </ItemsCarousel>
+            </div>
+        </div>
+    );
+}
+
+function AnimeItem(props){
+    return(
+        <div>
+             <div className={Styles.Anime__item}>
+                <img src={props.image}></img>
+                <h1>Attack Of Tittans</h1>
+            </div>
+        </div>
+    )
+}
+
+function AnimeSection() {
+    return(
+        <div className={Styles.Anime}>
+            <h1>Best Anime of the last time</h1>
+            <AnimeSlider></AnimeSlider>
+            <AnimeSlider></AnimeSlider>
+        </div>
+    )
+}
+
+function MangaItem(props){
+    return(
+        <div className={Styles.Manga__item}>
+            <img src={props.image}></img>
+            <h1>Attack Of Tittans</h1>
+        </div>
+    )
+}
+
+function MangaSlider(){
+    const [activeItemIndex, setActiveItemIndex] = useState(0);
+    const chevronWidth = 150;
+
+    return(
+        <div className={Styles.Manga__Slider}>
+            <div style={{ padding: `0 ${chevronWidth}px` }}>
+                <ItemsCarousel
+                    requestToChangeActive={setActiveItemIndex}
+                    activeItemIndex={activeItemIndex}
+                    numberOfCards={5}
+                    gutter={20}
+                    leftChevron={<button className={Styles.SliderButton}>{'<'}</button>}
+                    rightChevron={<button className={Styles.SliderButton}>{'>'}</button>}
+                    outsideChevron
+                    chevronWidth={chevronWidth}
+                >
+                    <MangaItem image= {Title1}/>
+                    <MangaItem image= {Title2}/>
+                    <MangaItem image= {Title4}/>
+                    <MangaItem image= {Title3}/>
+                    <MangaItem image= {Title4}/>
+                    <MangaItem image= {Title1}/>
+                    <MangaItem image= {Title1}/>
+                </ItemsCarousel>
+            </div>
+        </div>
+    );
+}
 
 function MangaSection(){
     return(
         <div className={Styles.Manga}>
-
+            <h1>Best Manga of the last time</h1>
+            <MangaSlider></MangaSlider>
+            <MangaSlider></MangaSlider>
+            
+            <button className={Styles.Manga__butt}>Show all</button>
         </div>
     )
 }
@@ -32,11 +136,13 @@ function Statistic(){
 }
 
 export default function Home(){
+
     return(
         <div>
-            <Head></Head>
-            <Statistic></Statistic>
+            <Head></Head>   
             <MangaSection></MangaSection>
+            <AnimeSection></AnimeSection>
         </div>
+        
     )
 }
